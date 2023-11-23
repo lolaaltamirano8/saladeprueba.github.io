4178e1eb813bdf80178725b3d84318a10c5e2e2a
lolaaltamirano8 <147611235+lolaaltamirano8@users.noreply.github.com>
2023-11-23 12:01:27 +0100

    Update gesture.js

--- gesture.js
+++ gesture.js
@@ -1,44 +1,17 @@
 // gesture.js
-let initialDistance = 0;
+let initialScale = 1;
+const hammer = new Hammer(document.body);
 
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
+hammer.get('pinch').set({
+  enable: true
+});
 
-function handleTouchStart(event) {
-  if (event.touches.length === 2) {
-    const touch1 = event.touches[0];
-    const touch2 = event.touches[1];
-    initialDistance = getDistance(touch1, touch2);
-  }
-}
+hammer.on('pinchstart', function (event) {
+  initialScale = flower ? flower.scale.x : 1;
+});
 
-function handleTouchMove(event) {
-  if (event.touches.length === 2) {
-    const touch1 = event.touches[0];
-    const touch2 = event.touches[1];
-    const currentDistance = getDistance(touch1, touch2);
-
-    if (currentDistance > initialDistance) {
-      // Aumentar escala
-      scaleModel(1.02); // Puedes ajustar el factor de escala según tus necesidades
-    } else if (currentDistance < initialDistance) {
-      // Reducir escala
-      scaleModel(0.98); // Puedes ajustar el factor de escala según tus necesidades
-    }
-
-    initialDistance = currentDistance;
-  }
-}
-
-function getDistance(touch1, touch2) {
-  const dx = touch1.clientX - touch2.clientX;
-  const dy = touch1.clientY - touch2.clientY;
-  return Math.sqrt(dx * dx + dy * dy);
-}
-
-function scaleModel(factor) {
-  // Escala el modelo aquí según el factor proporcionado
+hammer.on('pinch', function (event) {
   if (flower) {
-    flower.scale.multiplyScalar(factor);
+    flower.scale.set(initialScale * event.scale, initialScale * event.scale, initialScale * event.scale);
   }
-}
+});
