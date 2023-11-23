4b5edf702aff895dd5728f3eb7e6967696e4469e
lolaaltamirano8 <147611235+lolaaltamirano8@users.noreply.github.com>
2023-11-23 12:06:59 +0100

    Update gesture.js

--- gesture.js
+++ gesture.js
@@ -1,17 +1,32 @@
 // gesture.js
 let initialScale = 1;
-const hammer = new Hammer(document.body);
 
-hammer.get('pinch').set({
-  enable: true
-});
-
-hammer.on('pinchstart', function (event) {
-  initialScale = flower ? flower.scale.x : 1;
-});
-
-hammer.on('pinch', function (event) {
+function handlePinch(event) {
   if (flower) {
     flower.scale.set(initialScale * event.scale, initialScale * event.scale, initialScale * event.scale);
   }
-});
+}
+
+function setupGestures(camera, renderer) {
+  const hammer = new Hammer(renderer.domElement);
+  const gestures = new THREE.GestureHandler(camera, renderer.domElement);
+
+  hammer.get('pinch').set({
+    enable: true
+  });
+
+  hammer.on('pinchstart', function (event) {
+    initialScale = flower ? flower.scale.x : 1;
+  });
+
+  hammer.on('pinch', handlePinch);
+
+  gestures.addEventListener('pinchstart', function (event) {
+    initialScale = flower ? flower.scale.x : 1;
+  });
+
+  gestures.addEventListener('pinchmove', handlePinch);
+}
+
+// Additional code for other gestures can be added here if needed
+
