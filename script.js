a4881f7e9e62f2fa85666080af2d828c0ac8b503
lolaaltamirano8 <147611235+lolaaltamirano8@users.noreply.github.com>
2023-11-23 12:46:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,111 @@
 //Código para detectar superficie, detectar al usuario tocando la pantalla (y dónde) y colocando el modelo únicamente con el primer toque.
-let modelPlaced = false; // Variable para rastrear si el modelo ya se colocó
+let modelPlaced = false;
+let flower;
+let pinchScaleStart = 0;
+let pinchScaleCurrent = 1;
 
-    async function activateXR() {
-      // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
-      const canvas = document.createElement("canvas");
-      document.body.appendChild(canvas);
-      const gl = canvas.getContext("webgl", {
-        xrCompatible: true
-      });
+async function activateXR() {
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+  const gl = canvas.getContext("webgl", {
+    xrCompatible: true
+  });
 
-      const scene = new THREE.Scene();
+  const scene = new THREE.Scene();
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+  directionalLight.position.set(10, 15, 10);
+  scene.add(directionalLight);
 
-      // Add directional light to the scene
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
-      directionalLight.position.set(10, 15, 10);
-      scene.add(directionalLight);
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    preserveDrawingBuffer: true,
+    canvas: canvas,
+    context: gl
+  });
+  renderer.autoClear = false;
 
-      // Set up the WebGLRenderer, which handles rendering to the session's base layer.
-      const renderer = new THREE.WebGLRenderer({
-        alpha: true,
-        preserveDrawingBuffer: true,
-        canvas: canvas,
-        context: gl
-      });
-      renderer.autoClear = false;
+  const camera = new THREE.PerspectiveCamera();
+  camera.matrixAutoUpdate = false;
 
-      // The API directly updates the camera matrices.
-      // Disable matrix auto updates so three.js doesn't attempt
-      // to handle the matrices independently.
-      const camera = new THREE.PerspectiveCamera();
-      camera.matrixAutoUpdate = false;
+  const session = await navigator.xr.requestSession("immersive-ar", {
+    requiredFeatures: ['hit-test']
+  });
+  session.updateRenderState({
+    baseLayer: new XRWebGLLayer(session, gl)
+  });
 
-      // Initialize a WebXR session using "immersive-ar" with hit-test feature.
-      const session = await navigator.xr.requestSession("immersive-ar", {
-        requiredFeatures: ['hit-test']
-      });
-      session.updateRenderState({
-        baseLayer: new XRWebGLLayer(session, gl)
-      });
+  const hitTestSourceSpace = await session.requestReferenceSpace('viewer');
+  const hitTestSource = await session.requestHitTestSource({
+    space: hitTestSourceSpace
+  });
 
-      // Initialize hit-test source
-      const hitTestSourceSpace = await session.requestReferenceSpace('viewer');
-      const hitTestSource = await session.requestHitTestSource({
-        space: hitTestSourceSpace
-      });
+  const referenceSpace = await session.requestReferenceSpace('local');
 
-      // A 'local' reference space has a native origin that is located
-      // near the viewer's position at the time the session was created.
-      const referenceSpace = await session.requestReferenceSpace('local');
+  const loader = new THREE.GLTFLoader();
+  let reticle;
+  loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", function (gltf) {
+    reticle = gltf.scene;
+    reticle.visible = false;
+    scene.add(reticle);
+  });
 
-      // Create a GLTF loader
-      const loader = new THREE.GLTFLoader();
+  loader.load("https://raw.githubusercontent.com/lolaaltamirano8/lolaaltamirano8.github.io/main/prensa_husillo.gltf", function (gltf) {
+    flower = gltf.scene;
+    setupGestures(camera, renderer, flower);
+  });
 
-      // Load reticle model
-      let reticle;
-      loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", function (gltf) {
-        reticle = gltf.scene;
-        reticle.visible = false;
-        scene.add(reticle);
-      });
+  const onXRFrame = (time, frame) => {
+    session.requestAnimationFrame(onXRFrame);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer)
 
-      // Load sunflower model
-      let flower;
-      loader.load("https://raw.githubusercontent.com/lolaaltamirano8/lolaaltamirano8.github.io/main/prensa_husillo.gltf", function (gltf) {
-        flower = gltf.scene;
-          setupGestures(camera, renderer, flower);
-      });
+    const pose = frame.getViewerPose(referenceSpace);
+    if (pose) {
+      const view = pose.views[0];
+      const viewport = session.renderState.baseLayer.getViewport(view);
+      renderer.setSize(viewport.width, viewport.height)
 
-      // Create a render loop that allows us to draw on the AR view.
-      const onXRFrame = (time, frame) => {
-        // Queue up the next draw request.
-        session.requestAnimationFrame(onXRFrame);
+      camera.matrix.fromArray(view.transform.matrix)
+      camera.projectionMatrix.fromArray(view.projectionMatrix);
+      camera.updateMatrixWorld(true);
 
-        // Bind the graphics framebuffer to the baseLayer's framebuffer
-        gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer)
-
-        // Retrieve the pose of the device.
-        // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
-        const pose = frame.getViewerPose(referenceSpace);
-        if (pose) {
-          // In mobile AR, we only have one view.
-          const view = pose.views[0];
-
-          const viewport = session.renderState.baseLayer.getViewport(view);
-          renderer.setSize(viewport.width, viewport.height)
-
-          // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-          camera.matrix.fromArray(view.transform.matrix)
-          camera.projectionMatrix.fromArray(view.projectionMatrix);
-          camera.updateMatrixWorld(true);
-
-          // Perform hit testing using the viewer as origin.
-          const hitTestResults = frame.getHitTestResults(hitTestSource);
-          if (hitTestResults.length > 0 && reticle) {
-            const hitPose = hitTestResults[0].getPose(referenceSpace);
-            reticle.visible = true;
-            reticle.position.set(hitPose.transform.position.x, hitPose.transform.position.y, hitPose.transform.position.z)
-            reticle.updateMatrixWorld(true);
-          }
-
-          // Render the scene with THREE.WebGLRenderer.
-          renderer.render(scene, camera)
-        }
+      const hitTestResults = frame.getHitTestResults(hitTestSource);
+      if (hitTestResults.length > 0 && reticle) {
+        const hitPose = hitTestResults[0].getPose(referenceSpace);
+        reticle.visible = true;
+        reticle.position.set(hitPose.transform.position.x, hitPose.transform.position.y, hitPose.transform.position.z)
+        reticle.updateMatrixWorld(true);
       }
-      session.requestAnimationFrame(onXRFrame);
 
-      // Event listener for the "select" event (user pressing the screen)
-      session.addEventListener("select", (event) => {
-        if (flower && !modelPlaced == true) { // Verificar si el modelo existe y aún no se ha colocado
-          const clone = flower.clone();
-          clone.position.copy(reticle.position);
-          scene.add(clone);
-          modelPlaced = true; // Actualizar la variable para indicar que el modelo se ha colocado
-        }
-      });
+      // Escalar el modelo con el gesto de pellizco
+      if (pinchScaleStart !== undefined && pinchScaleCurrent !== undefined) {
+        const pinchScaleDelta = pinchScaleCurrent - pinchScaleStart;
+        const scale = Math.max(0.1, pinchScaleDelta + 1);
+        flower.scale.set(scale, scale, scale);
+      }
+
+      renderer.render(scene, camera)
     }
+  }
+  session.requestAnimationFrame(onXRFrame);
+
+  session.addEventListener("selectstart", (event) => {
+    pinchScaleStart = event.inputSource.gamepad.buttons[1].value; // assuming button 1 is the touchpad or trigger
+  });
+
+  session.addEventListener("selectend", (event) => {
+    pinchScaleStart = undefined;
+  });
+
+  session.addEventListener("select", (event) => {
+    if (flower && !modelPlaced) {
+      const clone = flower.clone();
+      clone.position.copy(reticle.position);
+      scene.add(clone);
+      modelPlaced = true;
+    }
+  });
+}
+
+function setupGestures(camera, renderer, model) {
+  // Aquí puedes agregar configuración adicional para la detección de gestos si es necesario.
+}
